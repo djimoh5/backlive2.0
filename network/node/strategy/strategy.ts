8b01d71adfda9ef6d070b0caaa2801d4400627b3
djimoh5 <deji.jimoh@gmail.com>
2016-09-03 15:36:15 -0500

    added calculator

--- network/node/strategy/strategy.ts
+++ network/node/strategy/strategy.ts
@@ -1,53 +1,30 @@
 import {BaseNode} from '../base-node'
 import {DataEvent, DataSubscriptionEvent} from '../../lib/events/app-event';
-import {IDataHandler} from '../data-handler/data-handler';
 
 import {Strategy as StrategyModel} from '../../../app/service/model/strategy.model';
 import {Operator, Indicator, Param} from '../../../app/service/model/indicator.model';
-import {Common} from '../../../app//utility/common';;
+import {Common} from '../../../app//utility/common';
+
+import {Calculator} from './calculator';
 
 export class Strategy extends BaseNode {
     allIndicators: Indicator[];
+    calculator: Calculator;
     
     constructor(private model: StrategyModel) {
         super();
         var data = model.data;
         this.allIndicators = data.indicators.long.concat(data.indicators.short, data.exposure.long, data.exclusions);
         
+        this.calculator = new Calculator(this.allIndicators);
+        
         this.subscribe(DataEvent, (event: DataEvent) => this.processData(event));
-        this.notify(new DataSubscriptionEvent({ params: this.getIndicatorParams(), startDate: data.startYr, endDate: data.endYr, 
+        this.notify(new DataSubscriptionEvent({ params: this.calculator.getIndicatorParams(), startDate: data.startYr, endDate: data.endYr, 
             entities: (data.universeTkrs.incl === 1 && data.universeTkrs.tkrs && data.universeTkrs.tkrs.length > 0) ? data.universeTkrs.tkrs : null }));
     }
     
     processData(event: DataEvent) {
         console.log('Strategy received event');
-    }
-    
-    getIndicatorParams(): Param[] {
-        var params: [number, string][] = [];
-        
-        this.allIndicators.forEach(indicator => {
-            this.getIndicatorParamsHelper(indicator, params);
-        });
-        
-        return params;
-    }
-
-    private getIndicatorParamsHelper(indicator: Indicator, params: Param[]) {
-        for(var i = 0, cnt = indicator.vars.length; i < cnt; i++) {
-			var obj = indicator.vars[i];
-			
-			if(obj) {
-				if(!Common.isArray(obj)) {
-                    this.getIndicatorParamsHelper(<Indicator> obj, params);
-                }
-                else {
-					var type: number = parseInt(obj[0]);
-					var field: string = obj[1];
-					params.push([type, field]);					
-				}
-			}
-		}
     }
     
     getModel() {
